425ed85f911d22908dd27f833587e901ab64e2c5
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-02-12 20:52:45 -0500

    working on passengers array iteration 4

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,45 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
+    this.waitingList = [];
+    this.passengers = [];
+    this.requests = [];
+    this.intervalId;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.intervalId = setInterval(() => {
+      this.update();
+    } 1000);
+   }
+  stop() {
+    clearInterval(this.intervalId);
+  }
+  update() {
+    this.log();
+   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() { 
+    if(this.floor < this.MAXFLOOR) {
+      this.floor++;
+    }
+  }
+  floorDown() { 
+    if(this.floor > 0) {
+      this.floor--;
+    }
+  }
+  call(person) { 
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
+  }
+  log() { 
+    console.log(
+      `Direction: ${this.direction} 
+      Floor: ${this.floor}`
+    )}
 }
 
 module.exports = Elevator;
